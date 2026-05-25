8f5a2b97b6d14dbf4e1dd5fd188aff32a5dbcd0a
itzmarcos <100073108+itzmarcos@users.noreply.github.com>
2026-05-25 19:30:29 +0000

    Nivel novato

--- xadrez.c
+++ xadrez.c
@@ -8,15 +8,6 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
@@ -27,6 +18,66 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+
+    // =========================
+    // Movimento da Torre
+    // Utilizando FOR
+    // Torre: 5 casas para a direita
+    // =========================
+    
+    // Implementação de Movimentação da Torre
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    
+    int i;
+
+    printf("Movimento da Torre:\n");
+
+    for(i = 1; i <= 5; i++) {
+        printf("Direita\n");
+    }
+
+    // Linha em branco para separar os movimentos
+    printf("\n");
+
+
+    // =========================
+    // Movimento do Bispo
+    // Utilizando WHILE
+    // Bispo: 5 casas na diagonal
+    // (Cima e Direita)
+    // =========================
+
+    // Implementação de Movimentação do Bispo
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int contadorBispo = 1;
+
+    printf("Movimento do Bispo:\n");
+
+    while(contadorBispo <= 5) {
+        printf("Cima Direita\n");
+        contadorBispo++;
+    }
+
+    // Linha em branco para separar os movimentos
+    printf("\n");
+
+    // Implementação de Movimentação da Rainha
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // =========================
+    // Movimento da Rainha
+    // Utilizando DO-WHILE
+    // Rainha: 8 casas para a esquerda
+    // =========================
+
+    int contadorRainha = 1;
+
+    printf("Movimento da Rainha:\n");
+
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while(contadorRainha <= 8);
+
 
     return 0;
 }
